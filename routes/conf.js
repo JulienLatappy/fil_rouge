86bf644c23b1a0b83736b64e99384f16e1155387
JulienLatappy <42190519+JulienLatappy@users.noreply.github.com>
2018-11-26 10:55:07 +0100

    Delete conf.js

--- routes/conf.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql');
-
-const connection = mysql.createConnection({
-host:  'localhost', // adresse du serveur
-user:  'root', // le nom d'utilisateur
-password:  'Lapetete', // le mot de passe
-database:  'music', // le nom de la base de données
-});
-module.exports = connection;
